264c538e4dc840aba7a20f0e81e88c8674a983de
OscarVegaV <os_ar1@hotmail.com>
2021-01-29 14:46:58 -0600

    arrays_and_methods

---
+++ 21_methods.rb
@@ -0,0 +1,21 @@
+def twice(thing)
+    thing + thing
+end
+
+test_variable = twice(4)
+puts
+print test_variable
+
+test_variable = twice([2,4,6])
+puts
+print test_variable
+
+
+test_variable = twice("Pura vida")
+puts
+print test_variable
+
+
+test_variable = twice()
+puts
+print test_variable
---
+++ 22_methods2.rb
@@ -0,0 +1,9 @@
+
+def say_hello(name,last_name = "rojas")
+    horoscope = "capricornio"
+    "Hola #{name.capitalize} #{last_name.capitalize}, pura vida #{horoscope.capitalize}"
+
+end
+
+puts say_hello("justin","bieber" )
+puts say_hello ("justin")
